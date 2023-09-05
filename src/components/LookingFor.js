f57f81511b2bdf92a5d61615597357317ca60cb1
vinay-eng <singhvinay2017@gmail.com>
2023-09-05 22:45:20 +0530

    rent,buy fixed

--- src/components/LookingFor.js
+++ src/components/LookingFor.js
@@ -7,13 +7,13 @@ import "../style/lookingFor.css"
 function LookingFor(){
     return (
         <div className='looking'>
-            <h2>WHAT ARE YOU LOOKING FOR?</h2>
+            <h2>WHAT ARE YOU LOOKING FOR?</h2> 
             <div className='looking_box'>
                 <div className='lookingForRent rent_and_buy_box'>
                     <h1>RENT</h1>
                 </div>
                 <div className='lookingForBuy rent_and_buy_box'>
-                <h1>BUY</h1>
+                    <h1>BUY</h1>
                 </div>
             </div>
             
