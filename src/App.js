449c673adfddfe84d35a65b6e8945549b5aa6771
vinay-eng <singhvinay2017@gmail.com>
2023-09-05 18:01:02 +0530

    Trending property started

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import NAVBAR from './components/navbar'
 import Contact from './components/contact'
 import SearchProperty from './components/SearchProperty'
 import LookingFor from "./components/LookingFor"
+// import ContactUs from "./components/contactUs"
+import TrendingProjects from './components/TrendingProjects';
+
+
 import background from "./assets/background.jpg";
+
+
+
 // import './style/app.css'
 function App() {
   return (
@@ -14,9 +21,10 @@ function App() {
         <NAVBAR/>
         <SearchProperty/>
         
-        {/* <Body/> */}
       </div>
       <LookingFor/>
+      <TrendingProjects/>
+      {/* <ContactUs/> */}
     </div>
     
   );
